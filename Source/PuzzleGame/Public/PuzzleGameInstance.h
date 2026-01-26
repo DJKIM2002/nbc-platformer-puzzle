99b2cb5f80fb17d1b761b521f2715f95494ed599
djkim12 <agwx1223@naver.com>
2026-01-26 20:14:19 +0900

    Add PuzzleGameInstance class and source files
    Introduces UPuzzleGameInstance class derived from UGameInstance, with header and source files created for future game instance logic.

---
+++ Source/PuzzleGame/Public/PuzzleGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "PuzzleGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PUZZLEGAME_API UPuzzleGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
